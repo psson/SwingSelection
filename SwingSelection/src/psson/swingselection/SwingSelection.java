def6345c5145e4001af7b91fabeaf9981205aeeb
Andreas Pettersson <>
2015-03-18 20:12:21 +0100

    Started adding members and functions.

--- SwingSelection/src/psson/swingselection/SwingSelection.java
+++ SwingSelection/src/psson/swingselection/SwingSelection.java
@@ -24,7 +24,9 @@
 package psson.swingselection;
 
 import java.awt.Container;
+import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
 import javax.swing.JComponent;
 
 /**
@@ -34,19 +36,37 @@ import javax.swing.JComponent;
  */
 public class SwingSelection {
     
-    private final Container parent;
+//<editor-fold defaultstate="collapsed" desc="Members and constructors">
+    private static final int NUM_SELECTION_HANDLES = 8;
+    private static final int SELECTION_HANDLE_SIZE = 3;
+    
+    private final Container c;
+    private Rectangle[] handles;
     private final InternalSelection mySel;
+    
+    private ContainerMouseAdapter listener;
     
     /**
      * Creates a new SwingSelection and connects it to a parent Container
-     * @param parent a Swing Container where the selections will be done
+     * @param c a Swing Container where the selections will be done
      */
-    public SwingSelection( Container parent ) {
-        this.parent = parent;
+    public SwingSelection( Container c ) {
+        this.c = c;
         mySel = new InternalSelection();
-        this.parent.add(mySel);
+        this.c.add( mySel );
+        
+        handles = new Rectangle[ NUM_SELECTION_HANDLES ];
+        initSelectionHandles();
+        
+        listener = new ContainerMouseAdapter();
+        
+        c.addMouseListener( listener );
+        c.addMouseMotionListener( listener );
+        
     }
+//</editor-fold>
     
+//<editor-fold defaultstate="collapsed" desc="Access functions">
     /**
      * Sets the size and position of the selection
      * @param r a Rectangle with the size and position of the selection
@@ -64,18 +84,95 @@ public class SwingSelection {
     }
     
     /**
+     * Checks whether the selection contains the provided coordinates
+     * @param x the x coordinate to check
+     * @param y the y coordinate to check
+     * @return true if the selection contains x,y
+     */
+    public boolean contains( int x, int y ) {
+        //TODO The documentation for contains( Point p ) states that the Point
+        // is taken to be relative to the coordinates of the component
+        // Check if this is also true for this function
+        // If true, modify input here, otherwise check and modify
+        // contains( Point p ) if needed.
+        return mySel.contains( x, y );
+    }
+    
+    /**
+     * Checks whether the selection contains the point
+     * @param p the point to check
+     * @return true if the selection contains p
+     */
+    public boolean contains( Point p ) {
+        // Wrapper for contains( int x, int y)
+        //TODO See TODO for contains( int x, int y )
+        // Check other function first to avoid needlessly redoing
+        // coordinate adjustments
+        return this.contains( (int)p.getX(), (int)p.getY() );
+    }
+    
+    /**
      * Sets the visibility of the selection
      * @param visible true to make selection visible, false to make it invisible
      */
     public void setVisible( boolean visible ) {
         mySel.setVisible( visible );
-        parent.repaint();
+        c.repaint();
+    }
+//</editor-fold>
+    
+    /**
+     * Calculates and sets the selection handles associated with the selection.
+     */
+    private void setSelectionHandles() {
+        
+        // Get limits of selection for easier
+        int width = mySel.getWidth();       // Width
+        int height = mySel.getHeight();     // Height
+        int left = mySel.getX();            // Left side
+        int right = left + width;           // Right side
+        int top = mySel.getY();             // Top
+        int bottom = top + height;          // Bottom
+        
+        // Upper left corner handle
+        handles[0].setBounds( left, top, SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE );
+        // Upper right corner handle
+        handles[1].setBounds( right - SELECTION_HANDLE_SIZE, top, SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE );
+        // Lower left corner handle
+        handles[2].setBounds( left, bottom - SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE );
+        // Lower right corner handle
+        handles[3].setBounds( right - SELECTION_HANDLE_SIZE, bottom - SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE, SELECTION_HANDLE_SIZE );
+        // Upper edge handle
+        handles[4].setBounds( left, top, width, SELECTION_HANDLE_SIZE );
+        // Bottom edge handle
+        handles[5].setBounds( left, bottom - SELECTION_HANDLE_SIZE, width, SELECTION_HANDLE_SIZE );
+        // Left edge handle
+        handles[6].setBounds( left, top, SELECTION_HANDLE_SIZE, height);
+        // Right edge handle
+        handles[7].setBounds( left - SELECTION_HANDLE_SIZE, top, SELECTION_HANDLE_SIZE, height);
+    }
+    
+    /**
+     * Initializes the array of selection handles to Rectangles
+     */
+    private void initSelectionHandles() {
+        
+        for ( int i = 0 ; i < NUM_SELECTION_HANDLES ; i++ ) {
+            handles[ i ] = new Rectangle();
+        }
     }
     
     /**
      * Internal class that is the actual component added to the parent Container.
      */
     private class InternalSelection extends JComponent {
+        
+    }
+    
+    /**
+     * Internal MouseAdapter class
+     */
+    private class ContainerMouseAdapter extends MouseAdapter {
         
     }
     
